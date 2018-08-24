8a81362d7489be52abffa051cd102c5d6ac4092d
Cristian Danilo Ordoñez <cristian.ordonez@globant.com>
2018-08-24 14:33:03 -0500

    first estructure

---
+++ src/app/pages/progress/progress.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styles: []
+})
+export class ProgressComponent implements OnInit {
+
+  progreso: number = 50;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeValue(valor) {
+    console.log(valor);
+
+    if (this.progreso >= 100 && valor > 0 ) {
+      this.progreso = 100;
+      return;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+    this.progreso = this.progreso + valor;
+  }
+
+}
